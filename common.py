ab69e0a2f65bab0cb71d83d9744e206f7e90224e
Min Cai <min.cai.china@gmail.com>
2017-05-19 06:57:31 +0800

    Update scripts.

--- common.py
+++ common.py
@@ -2,12 +2,12 @@ bench_and_trace_file_name_range = [
     # ('simple_pthread', 'test_traces/simple_pthread.trace.21454.0'),
 
     ('blackscholes', 'traces/blackscholes.trace.4.txt'),
-    # ('bodytrack', 'traces/bodytrack.trace.4.txt'),
-    # ('canneal', 'traces/canneal.trace.4.txt'),
-    # ('fluidanimate', 'traces/fluidanimate.trace.4.txt'),
-    # ('freqmine', 'traces/freqmine.trace.4.txt'),
-    # ('streamcluster', 'traces/streamcluster.trace.4.txt'),
-    # ('x264', 'traces/x264.trace.4.txt'),
+    ('bodytrack', 'traces/bodytrack.trace.4.txt'),
+    ('canneal', 'traces/canneal.trace.4.txt'),
+    ('fluidanimate', 'traces/fluidanimate.trace.4.txt'),
+    ('freqmine', 'traces/freqmine.trace.4.txt'),
+    ('streamcluster', 'traces/streamcluster.trace.4.txt'),
+    ('x264', 'traces/x264.trace.4.txt'),
 ]
 
 # max_cycles = 100000000
@@ -15,9 +15,9 @@ max_cycles = 100000
 num_cores = 64
 
 data_packet_injection_rate_range = [
-    # 0.05,
-    # 0.1,
-    # 0.15,
+    0.05,
+    0.1,
+    0.15,
     0.2,
     0.25,
 ]
@@ -27,23 +27,23 @@ ant_packet_injection_rate = 0.0002
 aco_selection_alpha_range = [
     0.30,
     # 0.35,
-    # 0.40,
+    0.40,
     # 0.45,
-    # 0.50,
+    0.50,
     # 0.55,
-    # 0.60,
+    0.60,
     # 0.65,
-    # 0.70,
+    0.70,
 ]
 
 reinforcement_factor_range = [
     0.0005,
     # 0.001,
-    # 0.002,
+    0.002,
     # 0.004,
-    # 0.008,
+    0.008,
     # 0.016,
-    # 0.032,
+    0.032,
     # 0.064,
 ]
 
