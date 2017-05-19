ad0e848ada4f9eb6c1d4ec749dbfc8cc97d6d5f4
Min Cai <min.cai.china@bjut.edu.cn>
2017-05-19 08:39:20 +0800

    Update scripts and results.

--- analyze.py
+++ analyze.py
@@ -3,31 +3,13 @@ from common import bench_and_trace_file_name_range, working_directory, max_cycle
     reinforcement_factor_range, data_packet_injection_rate_range
 from utils import parse_result, to_csv, generate_plot
 
-for data_packet_injection_rate in data_packet_injection_rate_range:
-    results = []
 
-    for bench, trace_file_name in bench_and_trace_file_name_range:
-            results.append(
-                parse_result(working_directory(bench, 64, 'OddEven', 'BufferLevel', max_cycles, data_packet_injection_rate, -1, -1),
-                             bench=bench)
-            )
-
-            for aco_selection_alpha in aco_selection_alpha_range:
-                for reinforcement_factor in reinforcement_factor_range:
-                    results.append(
-                        parse_result(working_directory(bench, 64, 'OddEven', 'ACO', max_cycles, data_packet_injection_rate, aco_selection_alpha, reinforcement_factor),
-                                     bench=bench)
-                    )
-
-
+def results_to_csv(results, csv_file_name):
     def algorithm(r):
         if r.configs['Selection'] == 'ACO':
             return r.configs['Routing'] + '+' + r.configs['Selection'] + '(' + str(r.configs['AcoSelectionAlpha']) + ', ' + str(r.configs['ReinforcementFactor']) + ')'
         else:
             return r.configs['Routing'] + '+' + r.configs['Selection']
-
-
-    csv_file_name = 'results/general_' + str(data_packet_injection_rate) + '.csv'
 
     to_csv(csv_file_name, results, [
         ('Bench', lambda r: r.props['bench']),
@@ -52,6 +34,27 @@ for data_packet_injection_rate in data_packet_injection_rate_range:
         ('Average Payload Packet Delay', lambda r: r.stats['AveragePayloadPacketDelay']),
     ])
 
+
+for data_packet_injection_rate in data_packet_injection_rate_range:
+    results = []
+
+    for bench, _ in bench_and_trace_file_name_range:
+            results.append(
+                parse_result(working_directory(bench, 64, 'OddEven', 'BufferLevel', max_cycles, data_packet_injection_rate, -1, -1),
+                             bench=bench)
+            )
+
+            for aco_selection_alpha in aco_selection_alpha_range:
+                for reinforcement_factor in reinforcement_factor_range:
+                    results.append(
+                        parse_result(working_directory(bench, 64, 'OddEven', 'ACO', max_cycles, data_packet_injection_rate, aco_selection_alpha, reinforcement_factor),
+                                     bench=bench)
+                    )
+
+    csv_file_name = 'results/general_' + str(data_packet_injection_rate) + '.csv'
+
+    results_to_csv(results, csv_file_name)
+
     generate_plot(csv_file_name,
                   'results/simulation_time_' + str(data_packet_injection_rate), 'Bench', 'Simulation Time (Seconds)',
                   'Algorithm', 'Simulation Time (Seconds)')
@@ -70,4 +73,44 @@ for data_packet_injection_rate in data_packet_injection_rate_range:
 
     generate_plot(csv_file_name,
                   'results/average_payload_packet_delay_' + str(data_packet_injection_rate), 'Bench', 'Average Payload Packet Delay',
+                  'Algorithm', 'Average Payload Packet Delay')
+
+for bench, _ in bench_and_trace_file_name_range:
+    results = []
+
+    for data_packet_injection_rate in data_packet_injection_rate_range:
+        results.append(
+            parse_result(working_directory(bench, 64, 'OddEven', 'BufferLevel', max_cycles, data_packet_injection_rate, -1, -1),
+                         bench=bench)
+        )
+
+        for aco_selection_alpha in aco_selection_alpha_range:
+            for reinforcement_factor in reinforcement_factor_range:
+                results.append(
+                    parse_result(working_directory(bench, 64, 'OddEven', 'ACO', max_cycles, data_packet_injection_rate, aco_selection_alpha, reinforcement_factor),
+                                 bench=bench)
+                )
+
+    csv_file_name = 'results/general_' + bench + '.csv'
+
+    results_to_csv(results, csv_file_name)
+
+    generate_plot(csv_file_name,
+                  'results/simulation_time_' + bench, 'Data Packet Injection Rate', 'Simulation Time (Seconds)',
+                  'Algorithm', 'Simulation Time (Seconds)')
+
+    generate_plot(csv_file_name,
+                  'results/throughput_' + bench, 'Data Packet Injection Rate', 'Throughput',
+                  'Algorithm', 'Throughput')
+
+    generate_plot(csv_file_name,
+                  'results/average_packet_delay_' + bench, 'Data Packet Injection Rate', 'Average Packet Delay',
+                  'Algorithm', 'Average Packet Delay')
+
+    generate_plot(csv_file_name,
+                  'results/payload_throughput_' + bench, 'Data Packet Injection Rate', 'Payload Throughput',
+                  'Algorithm', 'Payload Throughput')
+
+    generate_plot(csv_file_name,
+                  'results/average_payload_packet_delay_' + bench, 'Data Packet Injection Rate', 'Average Payload Packet Delay',
                   'Algorithm', 'Average Payload Packet Delay')
