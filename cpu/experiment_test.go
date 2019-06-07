917b85883fadd23836798d804ab06d6f299a26e5
Min Cai <min.cai.china@gmail.com>
2019-06-07 11:59:33 +0800

    Fix warnings.

--- cpu/experiment_test.go
+++ cpu/experiment_test.go
@@ -9,7 +9,7 @@ func TestMstBaseline(t *testing.T) {
 	var config = NewCPUConfig("../test_results/real/mst_baseline")
 
 	config.ContextMappings = append(config.ContextMappings,
-		NewContextMapping(0, "../benchmarks/Olden_Custom1/mst/baseline/mst.mips", "1000"))
+		NewContextMapping(0, "../benchmarks/Olden_Custom1/mst/baseline/mst.mips", "100"))
 
 	config.NumCores = 2
 	config.NumThreadsPerCore = 2
@@ -29,7 +29,7 @@ func TestMstHelperThreaded(t *testing.T) {
 	var config = NewCPUConfig("../test_results/real/mst_ht")
 
 	config.ContextMappings = append(config.ContextMappings,
-		NewContextMapping(0, "../benchmarks/Olden_Custom1/mst/ht/mst.mips", "1000"))
+		NewContextMapping(0, "../benchmarks/Olden_Custom1/mst/ht/mst.mips", "100"))
 
 	config.NumCores = 2
 	config.NumThreadsPerCore = 2
