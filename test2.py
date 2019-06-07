e8a8e043a02f5bfb3183fe9185e291eb5fc04371
Min Cai <min.cai.china@gmail.com>
2019-06-07 20:10:38 +0800

    Use multi class classification in test.py.

--- test2.py
+++ test2.py
@@ -1,31 +1,30 @@
 import numpy as np
 import pandas as pd
+from keras import metrics
 from keras.layers import LSTM, Dense
+from keras.metrics import top_k_categorical_accuracy
 from keras.models import Sequential
+from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
+
+def top_10_accuracy(y_true, y_pred):
+    return top_k_categorical_accuracy(y_true, y_pred, k=10)
+
 sequence_length = 1
 num_features = 2
-num_outputs = 1
 
-model = Sequential()
-model.add(LSTM(units=20, return_sequences=True, input_shape=(sequence_length, num_features)))
-model.add(LSTM(units=20))
-model.add(Dense(units=num_outputs))
-model.compile(optimizer='adam', loss='mae', metrics=['accuracy'])
-model.summary()
+file_name = 'test_results/real/mst_ht/l2_requests_trace.txt'
 
-file_name = "test_results/real/mst_ht/l2_requests_trace.txt"
-
-df = pd.read_csv(file_name, names=["thread_id", "pc", "type", "data_address"])
+df = pd.read_csv(file_name, names=['thread_id', 'pc', 'type', 'data_address'])
 
 df['pc'] = df['pc'].apply(int, base=16).apply(lambda x: int(x / 8))
 df['data_address'] = df['data_address'].apply(int, base=16).apply(lambda x: int(x / 8))
 df['data_address_delta'] = df.groupby('pc', sort=False)['data_address'].diff()
 df['id'] = df.index
 
-df = df[["thread_id", "pc", "data_address_delta"]]
+df = df[['thread_id', 'pc', 'data_address_delta']]
 
 df = df[df['data_address_delta'].notnull()]
 
@@ -53,13 +52,22 @@ train, test = train_test_split(df, test_size=0.15)
 train_X = train[:, :, :-1]
 train_Y = train[:, :, -1:]
 
-train_Y = train_Y.reshape(np.size(train_Y, 0), np.size(train_Y, 1))
+train_Y = to_categorical(train_Y.reshape(np.size(train_Y, 0), np.size(train_Y, 1)))
+
+num_outputs = np.size(train_Y, -1)
+
+model = Sequential()
+model.add(LSTM(units=20, return_sequences=True, input_shape=(sequence_length, num_features)))
+model.add(LSTM(units=20))
+model.add(Dense(units=num_outputs, activation='softmax'))
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy', top_10_accuracy])
+model.summary()
 
 model.fit(train_X, train_Y, batch_size=1, epochs=3)
 
 test_X = test[:, :, :-1]
 test_Y = test[:, :, -1:]
 
-test_Y = test_Y.reshape(np.size(test_Y, 0), np.size(test_Y, 1))
+test_Y = to_categorical(test_Y.reshape(np.size(test_Y, 0), np.size(test_Y, 1)))
 
-model.evaluate(test_X, test_Y, verbose=0)
+model.evaluate(test_X, test_Y, verbose=1)
