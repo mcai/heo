9b5523e638c8e9380d5a4d56f8a651911f50d6d1
Min Cai <min.cai.china@gmail.com>
2019-06-09 00:52:32 +0800

    Update test2.py.

--- test2.py
+++ test2.py
@@ -6,115 +6,116 @@ from keras.models import Sequential
 from keras.utils import plot_model
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
-def top_5_accuracy(y_true, y_pred):
-    return top_k_categorical_accuracy(y_true, y_pred, k=5)
+class DeepNet:
+    def __init__(self):
+        pass
 
+    def top_5_accuracy(self, y_true, y_pred):
+        return top_k_categorical_accuracy(y_true, y_pred, k=5)
 
-def top_10_accuracy(y_true, y_pred):
-    return top_k_categorical_accuracy(y_true, y_pred, k=10)
+    def top_10_accuracy(self, y_true, y_pred):
+        return top_k_categorical_accuracy(y_true, y_pred, k=10)
 
+    def run(self, file_name):
+        sequence_length = 1
+        num_features = 2
 
-sequence_length = 1
-num_features = 2
+        df = pd.read_csv(file_name, names=['thread_id', 'pc', 'type', 'data_address'])
 
-file_name = 'test_results/real/mst_ht/l2_requests_trace.txt'
+        df['pc'] = df['pc'].apply(int, base=16).apply(lambda x: int(x / 8))
+        df['data_address'] = df['data_address'].apply(int, base=16).apply(lambda x: int(x / 8))
+        df['data_address_delta'] = df.groupby('pc', sort=False)['data_address'].diff()
+        df['id'] = df.index
 
-df = pd.read_csv(file_name, names=['thread_id', 'pc', 'type', 'data_address'])
+        df = df[['thread_id', 'pc', 'data_address_delta']]
 
-df['pc'] = df['pc'].apply(int, base=16).apply(lambda x: int(x / 8))
-df['data_address'] = df['data_address'].apply(int, base=16).apply(lambda x: int(x / 8))
-df['data_address_delta'] = df.groupby('pc', sort=False)['data_address'].diff()
-df['id'] = df.index
+        df = df[df['data_address_delta'].notnull()]
 
-df = df[['thread_id', 'pc', 'data_address_delta']]
+        # for i in range(1, sequence_length):
+        #     df['data_address_delta_prev_' + str(i)] = df['data_address_delta'].shift(-i)
+        #
+        # df = df[df['data_address_delta'].notnull()]
+        # for i in range(1, sequence_length):
+        #     df = df[df['data_address_delta_prev_' + str(i)].notnull()]
 
-df = df[df['data_address_delta'].notnull()]
+        df = df.values
 
-# for i in range(1, sequence_length):
-#     df['data_address_delta_prev_' + str(i)] = df['data_address_delta'].shift(-i)
-#
-# df = df[df['data_address_delta'].notnull()]
-# for i in range(1, sequence_length):
-#     df = df[df['data_address_delta_prev_' + str(i)].notnull()]
+        df = df.reshape(df.shape[0], 1, df.shape[1])
 
-df = df.values
+        encoder_pc = LabelEncoder()
+        encoder_data_address_delta = LabelEncoder()
 
-df = df.reshape(df.shape[0], 1, df.shape[1])
+        df[:, :, 1] = encoder_pc.fit_transform(df[:, :, 1]).reshape(-1, 1)
+        df[:, :, -1] = encoder_data_address_delta.fit_transform(df[:, :, -1]).reshape(-1, 1)
 
-encoder_pc = LabelEncoder()
-encoder_data_address_delta = LabelEncoder()
+        train, test = train_test_split(df, test_size=0.3)
 
-df[:, :, 1] = encoder_pc.fit_transform(df[:, :, 1]).reshape(-1, 1)
-df[:, :, -1] = encoder_data_address_delta.fit_transform(df[:, :, -1]).reshape(-1, 1)
+        train_X = train[:, :, :-1]
+        train_Y = train[:, :, -1:]
 
-train, test = train_test_split(df, test_size=0.3)
+        one_hot_encoder_address_delta = OneHotEncoder(handle_unknown='ignore')
 
-train_X = train[:, :, :-1]
-train_Y = train[:, :, -1:]
+        train_Y = one_hot_encoder_address_delta.fit_transform(train_Y.reshape(train_Y.shape[0], train_Y.shape[1]))
 
-one_hot_encoder_address_delta = OneHotEncoder(handle_unknown='ignore')
+        num_classes = np.size(train_Y, -1)
 
-train_Y = one_hot_encoder_address_delta.fit_transform(train_Y.reshape(train_Y.shape[0], train_Y.shape[1]))
+        model = Sequential()
+        model.add(LSTM(units=50, return_sequences=True, input_shape=(sequence_length, num_features)))
+        model.add(Dropout(0.2))
+        model.add(LSTM(units=50, return_sequences=True))
+        model.add(Dropout(0.2))
+        model.add(LSTM(units=50))
+        model.add(Dropout(0.2))
+        model.add(Dense(units=num_classes, activation='softmax'))
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=[
+            'accuracy', self.top_5_accuracy, self.top_10_accuracy
+        ])
+        model.summary()
 
-num_classes = np.size(train_Y, -1)
+        # plot_model(model, to_file='model.png')
 
-model = Sequential()
-model.add(LSTM(units=50, return_sequences=True, input_shape=(sequence_length, num_features)))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50, return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50))
-model.add(Dropout(0.2))
-model.add(Dense(units=num_classes, activation='softmax'))
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=[
-    'accuracy', top_5_accuracy, top_10_accuracy
-])
-model.summary()
+        history = model.fit(train_X, train_Y, batch_size=4, epochs=30, verbose=2, validation_split=0.25)
 
-# plot_model(model, to_file='model.png')
+        # plt.plot(history.history['acc'])
+        # plt.plot(history.history['val_acc'])
+        # plt.title('Model Accuracy')
+        # plt.ylabel('Accuracy')
+        # plt.xlabel('Epoch')
+        # plt.legend(['Train', 'Validation'], loc='upper left')
+        # plt.show()
+        #
+        # plt.plot(history.history['loss'])
+        # plt.plot(history.history['val_loss'])
+        # plt.title('Model Loss')
+        # plt.ylabel('Loss')
+        # plt.xlabel('Epoch')
+        # plt.legend(['Train', 'Validation'], loc='upper left')
+        # plt.show()
+        #
+        # plt.plot(history.history['top_5_accuracy'])
+        # plt.plot(history.history['val_top_5_accuracy'])
+        # plt.title('Model Top 5 Accuracy')
+        # plt.ylabel('Top 5 Accuracy')
+        # plt.xlabel('Epoch')
+        # plt.legend(['Train', 'Validation'], loc='upper left')
+        # plt.show()
+        #
+        # plt.plot(history.history['top_10_accuracy'])
+        # plt.plot(history.history['val_top_10_accuracy'])
+        # plt.title('Model Top 10 Accuracy')
+        # plt.ylabel('Top 10 Accuracy')
+        # plt.xlabel('Epoch')
+        # plt.legend(['Train', 'Validation'], loc='upper left')
+        # plt.show()
 
-history = model.fit(train_X, train_Y, batch_size=4, epochs=30, verbose=2, validation_split=0.25)
+        test_X = test[:, :, :-1]
+        test_Y = test[:, :, -1:]
 
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('Model Accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
-plt.show()
+        test_Y = one_hot_encoder_address_delta.transform(test_Y.reshape(test_Y.shape[0], test_Y.shape[1]))
 
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model Loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
-plt.show()
+        result = model.evaluate(test_X, test_Y, verbose=2)
 
-plt.plot(history.history['top_5_accuracy'])
-plt.plot(history.history['val_top_5_accuracy'])
-plt.title('Model Top 5 Accuracy')
-plt.ylabel('Top 5 Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
-plt.show()
-
-plt.plot(history.history['top_10_accuracy'])
-plt.plot(history.history['val_top_10_accuracy'])
-plt.title('Model Top 10 Accuracy')
-plt.ylabel('Top 10 Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
-plt.show()
-
-test_X = test[:, :, :-1]
-test_Y = test[:, :, -1:]
-
-test_Y = one_hot_encoder_address_delta.transform(test_Y.reshape(test_Y.shape[0], test_Y.shape[1]))
-
-result = model.evaluate(test_X, test_Y, verbose=2)
-
-print('loss: {:0.4f}, acc: {:0.4f}, top_5_accuracy: {:0.4f}, top_10_accuracy: {:0.4f}'.format(result[0], result[1], result[2], result[3]))
+        print('loss: {:0.4f}, acc: {:0.4f}, top_5_accuracy: {:0.4f}, top_10_accuracy: {:0.4f}'.format(result[0], result[1], result[2], result[3]))
